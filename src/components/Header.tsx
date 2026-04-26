9f31575fbcc9133c027be3676b5d8a69fcdfcf33
Codemonk <codemonk@192-168-100-106.rev.bb.zain.com>
2026-04-26 13:46:41 +0300

    Update logo asset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4">
         <Link href="/" className="flex items-center" onClick={() => setOpen(false)}>
           <Image
-            src="/monkscanner-logo.svg?v=3"
+            src="/monkscanner-logo-v2.svg"
             alt="MonkScanner logo"
             width={240}
             height={70}
