41163edf8964d7f90172045472fb85950b4a513c
Codemonk <codemonk@192-168-100-106.rev.bb.zain.com>
2026-04-26 15:30:08 +0300

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Free QR Code Generator Online | Create Custom QR Codes | MonkScanner",
@@ -68,6 +69,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XXXXXXXXXX');
+  `}
+</Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
